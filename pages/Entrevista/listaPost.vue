d2f02443f5da4311bc0ebe2f60484d4a1768f86c
DomeErazo <69824677+DomeErazo@users.noreply.github.com>
2024-05-15 05:14:34 +0000

    prueba

--- pages/Entrevista/listaPost.vue
+++ pages/Entrevista/listaPost.vue
@@ -341,7 +341,7 @@ export default {
       
       let Npsic = this.$cookies.get("dataClient").nombre;
       let Apsic = this.$cookies.get("dataClient").apellido;
-      //FALTA CAPTURAR EL NOMBRE DEL PSICOLOGO
+      //FALTA CAPTURAR EL NOMBRE DEL PSICOLOGO y probar
 
       try {
         const res = await axios.get(`/api/mineria/entrevistaLi/${Npsic + " "+Apsic}
